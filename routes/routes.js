ca90fc306b077e31dbad2c9348c082e17622d0ad
ashwini <ashwiniyadav23@navgurukul.org>
2025-02-04 11:02:38 +0530

    updated code

--- routes/routes.js
+++ routes/routes.js
@@ -17,4 +17,41 @@ router.post('/post', async (req, res) => {
     }
 });
 
+router.get('/getAll', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`your email ${data.email} has been deleted..`)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.patch('/update/:id', async (req, res) => {
+    try {
+            id = req.params.id 
+            updatedData = req.body 
+            options =  { new: true } 
+        
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 module.exports = router;
